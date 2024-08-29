1a9cdc8df306436931d56628c3e433f34dea539f
akvine <redgooseman55@gmail.com>
2024-08-29 12:48:59 +0300

    init commit

---
+++ src/main/java/ru/akvine/qraft/QraftApplication.java
@@ -0,0 +1,13 @@
+package ru.akvine.qraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akvine/qraft/QraftApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.qraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
